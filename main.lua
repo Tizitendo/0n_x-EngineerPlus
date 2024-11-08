8054ca2584e0b786d44c2b47c65f3c78353102de
Onyx <50289874+Tizitendo@users.noreply.github.com>
2024-11-08 16:39:22 +0100

    code cleanup

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ local Turret = nil
 
 Initialize(function()
     Engineer = Survivor.find("ror-engineer")
-    
+
     Engineer:onInit(function(self)
         player = Player.get_client()
     end)
@@ -16,6 +16,8 @@ end)
 gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, result, args)
     if args[1].value.parent == player.value and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
         args[1].value.stun = 1
+
+        --change knockback to always be away from turret/player
         if Turret ~= nil and Instance.exists(Turret) then
             if args[1].value.x > Turret.x then
                 args[1].value.knockback_direction = 1
@@ -35,20 +37,21 @@ gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, re
 end)
 
 gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, result, args)
+    -- get turret object
     if result.value.object_index == gm.constants.oEngiTurretB then
-        Turret = result.value 
+        Turret = result.value
     end
+
+    --destroys the turret target after 6 seconds
     if result.value.object_index == gm.constants.oActorTargetPlayer then
-        local target, bool = Instance.find_all(Object.find("ror-ActorTargetPlayer"))
-        for k, v in pairs(target) do
-            
-            local function destroytarget()
-                Helper.log_struct(n.value)
+        local target = Instance.find_all(Object.find("ror-ActorTargetPlayer"))
+        local function destroytarget()
+            for k, v in pairs(target) do
                 if v.value.parent ~= nil and v.value.parent.object_name == "oEngiTurretB" then
                     v:destroy()
                 end
             end
-            Alarm.create(test, 360)
         end
+        Alarm.create(destroytarget, 360)
     end
 end)
