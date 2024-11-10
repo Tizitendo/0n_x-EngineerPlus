3bde03218e0c566edd4876f1055dc9eced9fdae0
Onyx <50289874+Tizitendo@users.noreply.github.com>
2024-11-10 11:36:01 +0100

    fixed error spam

--- main.lua
+++ main.lua
@@ -8,29 +8,31 @@ local Turret = nil
 Initialize(function()
     Engineer = Survivor.find("ror-engineer")
 
-    Engineer:onInit(function(self)
+    Callback.add("onPlayerInit", "EngineerPlus-onPlayerInit", function()
         player = Player.get_client()
     end)
 end)
 
 gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, result, args)
-    if args[1].value.parent == player.value and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
-        if args[1].value.stun < 0.5 then
-            args[1].value.stun = 0.5
-        end
-
-        --change knockback to always be away from turret/player
-        if Turret ~= nil and Instance.exists(Turret) then
-            if args[1].value.x > Turret.x then
-                args[1].value.knockback_direction = 1
-            else
-                args[1].value.knockback_direction = -1
+    if player.name == "Engineer" then
+        if args[1].value.parent == player.value and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
+            if args[1].value.stun < 0.5 then
+                args[1].value.stun = 0.5
             end
-        else
-            if args[1].value.x > player.value.x then
-                args[1].value.knockback_direction = 1
+
+            -- change knockback to always be away from turret/player
+            if Turret ~= nil and Instance.exists(Turret) then
+                if args[1].value.x > Turret.x then
+                    args[1].value.knockback_direction = 1
+                else
+                    args[1].value.knockback_direction = -1
+                end
             else
-                args[1].value.knockback_direction = -1
+                if args[1].value.x > player.value.x then
+                    args[1].value.knockback_direction = 1
+                else
+                    args[1].value.knockback_direction = -1
+                end
             end
         end
     end
@@ -42,7 +44,7 @@ gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, re
         Turret = result.value
     end
 
-    --destroys the turret target after 6 seconds
+    -- destroys the turret target after 6 seconds
     if result.value.object_index == gm.constants.oActorTargetPlayer then
         local target = Instance.find_all(Object.find("ror-ActorTargetPlayer"))
         local function destroytarget()
