e6352d8e9b9cf64cd743d7daebcc5155f62adb54
Onyx <50289874+Tizitendo@users.noreply.github.com>
2025-02-13 14:24:13 +0100

    fixed error

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
 
 Initialize(function()
     gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, result, args)
-        if args[1].value.parent.player_p_number ~= nil and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
+        if type(args[1].value.parent) ~= "number" and args[1].value.parent.player_p_number ~= nil and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
             local targets = Instance.find_all(Object.find("ror", "ActorTargetPlayer"))
             local minDistance = (args[1].value.x - targets[1].x)^2 + (args[1].value.y - targets[1].y)^2
             local closestTarget = targets[1]
