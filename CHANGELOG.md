b6d499ffd98678d0425aa084b9bb00e46e1eb4e6
Onyx <50289874+Tizitendo@users.noreply.github.com>
2025-02-13 13:50:57 +0100

    made mp compatible

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 1.1.0
+* made multiplayer compatible
+
+## 1.1.0
+* updated with rmt
+
 ## 1.0.1
 * Fixed error spam
 * Fixed mod not working in edge cases
--- main.lua
+++ main.lua
@@ -1,59 +1,39 @@
 log.info("Successfully loaded " .. _ENV["!guid"] .. ".")
-mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
-
-local Engineer = nil
-local player = nil
-local Turret = nil
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
 
 Initialize(function()
-    Engineer = Survivor.find("ror-engineer")
-
-    Callback.add("onPlayerInit", "EngineerPlus-onPlayerInit", function()
-        player = Player.get_client()
+    gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, result, args)
+        if args[1].value.parent.player_p_number ~= nil and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
+            local targets = Instance.find_all(Object.find("ror", "ActorTargetPlayer"))
+            local minDistance = (args[1].value.x - targets[1].x)^2 + (args[1].value.y - targets[1].y)^2
+            local closestTarget = targets[1]
+            for i = 2, #targets do
+                local Distance = (args[1].value.x - targets[i].x)^2 + (args[1].value.y - targets[i].y)^2
+                if minDistance > Distance then
+                    minDistance = Distance
+                    closestTarget = targets[i]
+                end
+            end
+            if args[1].value.x > closestTarget.x then
+                args[1].value.knockback_direction = 1
+            else
+                args[1].value.knockback_direction = -1
+            end
+        end
     end)
 end)
 
-gm.pre_script_hook(gm.constants.damager_attack_process, function(self, other, result, args)
-    if player.name == "Engineer" then
-        if args[1].value.parent == player.value and args[1].value.hit_effect == 1632.0 and args[1].value.knockback == 10 then
-            if args[1].value.stun < 0.5 then
-                args[1].value.stun = 0.5
-            end
-
-            -- change knockback to always be away from turret/player
-            if Turret ~= nil and Instance.exists(Turret) then
-                if args[1].value.x > Turret.x then
-                    args[1].value.knockback_direction = 1
-                else
-                    args[1].value.knockback_direction = -1
-                end
-            else
-                if args[1].value.x > player.value.x then
-                    args[1].value.knockback_direction = 1
-                else
-                    args[1].value.knockback_direction = -1
-                end
-            end
-        end
-    end
-end)
-
 gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, result, args)
-    -- get turret object
-    if result.value.object_index == gm.constants.oEngiTurretB then
-        Turret = result.value
-    end
-
     -- destroys the turret target after 6 seconds
-    if result.value.object_index == gm.constants.oActorTargetPlayer then
-        local target = Instance.find_all(Object.find("ror-ActorTargetPlayer"))
-        local function destroytarget()
-            for k, v in pairs(target) do
-                if v.value.parent ~= nil and v.value.parent.object_name == "oEngiTurretB" then
-                    v:destroy()
+    local function WaitForInit(result)
+        if result.object_index == gm.constants.oActorTargetPlayer and result.parent ~= nil and result.parent.object_index == gm.constants.oEngiTurretB then
+            local function destroytarget(target)
+                if target ~= nil and target.parent ~= nil and target.parent.object_index == gm.constants.oEngiTurretB then
+                    Instance.wrap(target):destroy()
                 end
             end
+            Alarm.create(destroytarget, 360, result)
         end
-        Alarm.create(destroytarget, 360)
     end
+    Alarm.create(WaitForInit, 1, result.value)
 end)
